6f96dcea499f3437eac6f32aa7636c1f4113ea8c
James Blades <jwkblades+git@gmail.com>
2020-08-31 12:36:44 -0600

    Clean up bin directory, which isn't needed.

--- bin/dup.py
+++
@@ -1,9 +0,0 @@
-#!/bin/python
-with open('output.txt') as f:
-    seen = set()
-    for line in f:
-        line_lower = line.lower()
-        if line_lower in seen:
-            print(line)
-        else:
-            seen.add(line_lower)
