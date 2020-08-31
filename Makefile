7a05db802d13cd9392d731826702517ef1dcf77e
James Blades <jwkblades+git@gmail.com>
2020-08-31 12:36:16 -0600

    Updated a lot of stuff for a more complete linux notification application.

--- Makefile
+++ Makefile
@@ -1,76 +1,15 @@
-PREFIX := 
-DIRECTORY ?= ${CURDIR}
-COMPONENTS := ${DIRECTORY}/cc
-DEPDIR := ${DIRECTORY}/build
-INCDIR := i
-SRCDIR := s
-LIBS := -lpthread -lssl -lcrypto -lstdc++fs -ldl -pthread -lnotify -lgdk_pixbuf-2.0 -lgio-2.0 -lgobject-2.0 -lglib-2.0
-FLAGS = -Wall -Wextra -Werror -std=c++17 -O2
-ifeq (${DEBUG},1)
-	FLAGS += -fprofile-arcs -ftest-coverage -ggdb
-	LIBS += -lgcov
-else
-	FLAGS += -DNO_TIMER
-endif
 CC := g++
-MANUAL_INC := -I${DIRECTORY}  -I/usr/include/gdk-pixbuf-2.0 -I/usr/include/glib-2.0 -I/usr/lib64/glib-2.0/include -I/usr/include/libpng16 -I/usr/include/libmount -I/usr/include/blkid -I/usr/include/uuid
+FLAGS := -Wall -Wextra -Werror -pedantic -std=c++20 -O2
+LIBS := $(shell pkg-config --cflags --libs libnotify gtk+-3.0)
 
+.PHONY: all clean
 
-
-EXES := $(patsubst ${DIRECTORY}/application/%, %, $(shell find ${DIRECTORY}/application/ -type d))
-MODULES := $(patsubst ${DIRECTORY}/modules/%, %, $(shell find ${DIRECTORY}/modules/ -type d))
-INC := ${MANUAL_INC} $(foreach directory, $(shell find ${DIRECTORY} -name "${INCDIR}" -a -type d), -I${directory})
-OBJS := $(shell find ${DIRECTORY} \( -wholename "*/${SRCDIR}/*.cpp" -or -wholename "*/*.md" \) -a -type f)
-OBJS := $(OBJS:.cpp=.o)
-OBJS := $(OBJS:.md=.o)
-OBJS := $(patsubst ${DIRECTORY}/%, ${DEPDIR}/%, ${OBJS})
-$(shell mkdir -p ${DEPDIR} >/dev/null)
-DEPFLAGS = -MT $@ -MMD -MF ${DEPDIR}/$*.d
-COMPILE.cc = ${CC} ${DEPFLAGS} ${FLAGS} ${INC} -c
-DEPFILES := $(patsubst %, %.d, $(basename ${OBJS}))
-
-${DEPDIR}/%.o: %.cpp
-${DEPDIR}/%.o: ${DIRECTORY}/%.cpp ${DEPDIR}/%.d
-	@mkdir -p $(shell dirname $@)
-	@echo "Compiling $<"
-	@${COMPILE.cc} ${OUTPUT_OPTION} $<
-
-${DEPDIR}/%.o: %.md
-	@mkdir -p $(shell dirname $@)
-	@echo "Translating $<"
-	@echo "Looking for: _binary_$(subst .,_,$(subst /,_,$<))_start to replace with   $(shell basename -s .md $<)Documentation"
-	@objcopy --binary i386 --input binary --output elf64-x86-64 --redefine-sym _binary_$(subst .,_,$(subst /,_,$<))_start=$(shell basename -s .md $<)Documentation $< $@
-
-PHONY_TARGETS = $(foreach exe, ${EXES}, $(shell if [ -e application/${exe}/Makefile ]; then echo ${exe}; fi))
-PHONY_TARGETS += $(foreach exe, ${MODULES}, $(shell if [ -e modules/${exe}/Makefile ]; then echo ${exe}; fi))
-.PHONY: all clean print-% ${PHONY_TARGETS}
-all: ${EXES} ${MODULES}
+all: notify
 
 clean:
-	@rm -rf ${DEPDIR} *.gcno *.dat
-	@rm -f $(if ${PREFIX}, $(addprefix ${PREFIX}-, ${EXES}), ${EXES})
-	@rm -f $(if ${PREFIX}, $(addprefix ${PREFIX}-, ${MODULES}), ${MODULES})
+	@rm -f notify
 
-print-%:
-	@echo $* = ${$*}
+notify: application/notify/main.cpp
+	@echo "Building ${@}"
+	@${CC} ${FLAGS} ${^} -o ${@} ${LIBS}
 
-${DEPDIR}/%.d: ;
-.PRECIOUS: ${DEPDIR}/%.d
-
--include ${DEPFILES}
-
-define linkTemplate
-${${1}}: %: $${DIRECTORY}/build/${2}/%/main.o $(filter-out %/main.o,${OBJS})
-	@if [ -e $${DIRECTORY}/${2}/$${@}/Makefile ]; then \
-		echo -e "\nChanging Directory to ${2}/$${@}"; \
-		cd ${2}/$${@}; make $${MAKEFLAGS} ${2}-$${@} && sync && \
-		mv ${2}-$${@} ../../$${@}; \
-	fi
-	@if [ ! -e $${DIRECTORY}/${2}/$${@}/Makefile ]; then \
-		echo -e "\nLinking $${@}"; \
-		${CC} ${FLAGS} ${INC} -o $(if ${PREFIX}, ${PREFIX}-$${@}, $${@}) ${LIBS} $$(shell bin/dependencyObjects $${@} 2>/dev/null); \
-	fi
-endef
-
-$(eval $(call linkTemplate,EXES,application))
-$(eval $(call linkTemplate,MODULES,modules))
--- application/notify/main.cpp
+++ application/notify/main.cpp
@@ -1,35 +1,178 @@
+#include <ios>
 #include <iostream>
 #include <cstdlib>
+#include <string>
 using namespace std;
 
+#include <chrono>
+#include <thread>
+#include <getopt.h>
+#include <unistd.h>
+
+#include <gtk/gtk.h>
 #include <libnotify/notify.h>
 
-void cb(void)
+bool notificationExitNow(bool set = false)
 {
-    cout << "Callback called!" << endl;
-	NotifyNotification * Hello = notify_notification_new ("This is a test", "A secondary notification triggered by the first.", "");
-	notify_notification_show (Hello, NULL);
-	g_object_unref(G_OBJECT(Hello));
+    static bool applicationValue = false;
+    if (set)
+    {
+        applicationValue = set;
+    }
+    return applicationValue;
 }
 
-int main()
+void teardown(void)
 {
-	notify_init ("Hello world!");
-	NotifyNotification * Hello = notify_notification_new ("Hello world", "This is an example notification.", "dialog-information");
-    notify_notification_add_action (Hello, "cb", "Callback", NOTIFY_ACTION_CALLBACK(cb), NULL, NULL);
-	notify_notification_show (Hello, NULL);
-	g_object_unref(G_OBJECT(Hello));
+    notificationExitNow(true);
+    gtk_main_quit();
+}
 
+void user_function(void)
+{
+    cout << "OK" << endl;
+    teardown();
+}
+
+void Close(NotifyNotification* notification, char* action, gpointer)
+{
+    if (std::string("default") == action)
+    {
+        user_function();
+        return;
+    }
+
+    int closeReason = notify_notification_get_closed_reason(notification);
+    cout << "Called close with " << notification << ", and action " << action << "; closed because of: " << closeReason << endl;
+
+    teardown();
+}
+
+void usage(void)
+{
+    cerr << "Usage:" << endl
+         << "    " << "notify [options] <TITLE> <DESCRIPTION>" << endl
+         << endl
+         << "Options:" << endl
+         << "    " << "--now-text|-n         The text to display for the default 'now' option, indicating to take immediate action." << endl
+         << "    " << "--later-text|-l       The text to display for the later action, indicating that a deferral is desired." << endl
+         << "    " << "--deferral-period|-d  The deferral period, in hours, until we should try again." << endl
+         << "    " << "--timeout|-t          Timeout for the notification, in minutes." << endl
+         << "    " << "--icon|-i             The desired icon name to be used." << endl;
+}
+
+int main(int argc, char** argv)
+{
+    std::string title("");
+    std::string description("");
+    std::string nowOption("Now");
+    std::string laterOption("Later");
+    std::string laterValue("1");
+    std::string nowValue("NOW");
+    std::string icon("dialog-information");
+    size_t timeoutMinutes(15);
+    bool laterSet(false);
+
+    static struct option longOptions[] = {
+        {"now-text", 1, 0, 'n'},
+        {"later-text", 1, 0, 'l'},
+        {"deferral-period", 1, 0, 'd'},
+        {"timeout", 1, 0, 't'},
+        {"icon", 1, 0, 'i'}
+    };
+
+    int optionIndex(0);
+    int index(1);
+
+    while (1)
+    {
+        int c = getopt_long(argc, argv, "n:l:d:t:", longOptions, &optionIndex);
+        if (c == -1)
+        {
+            break;
+        }
+
+        switch (c)
+        {
+            case 'n':
+                nowOption = optarg;
+                index += 2;
+                break;
+            case 'l':
+                laterOption = optarg;
+                index += 2;
+                break;
+            case 'd':
+                if (!laterSet)
+                {
+                    laterValue = optarg;
+                    laterSet = true;
+                }
+                index += 2;
+                break;
+            case 't':
+                timeoutMinutes = std::atoll(optarg);
+                index += 2;
+                break;
+            case 'i':
+                icon = optarg;
+                index += 2;
+                break;
+            default:
+                usage();
+                return 1;
+        }
+    }
+
+    if (index + 2 != argc)
+    {
+        usage();
+        return 1;
+    }
+
+    title = argv[index];
+    description = argv[index + 1];
+
+
+    gtk_init(&argc, &argv);
+
+	notify_init("agentNotifier");
+	NotifyNotification* notification = notify_notification_new(title.c_str(), description.c_str(), icon.c_str());
+
+    notify_notification_add_action(notification, laterValue.c_str(), laterOption.c_str(), Close, NULL, NULL);
+    notify_notification_add_action(notification, nowValue.c_str(), nowOption.c_str(), Close, NULL, NULL);
+
+    notify_notification_add_action(notification, "default", "OK", Close, NULL, NULL);
+    g_signal_connect(G_OBJECT(notification), "closed", user_function, notification);
+
+    notify_notification_set_urgency(notification, NOTIFY_URGENCY_CRITICAL);
+
+    std::thread timerThread([timeoutMinutes]() -> void {
+        std::chrono::high_resolution_clock::time_point timeoutTime = std::chrono::high_resolution_clock::now();
+        timeoutTime += std::chrono::minutes(timeoutMinutes);
+        bool timedOut = false;
+
+        while (!notificationExitNow())
+        {
+            if (std::chrono::high_resolution_clock::now() >= timeoutTime)
+            {
+                timedOut = true;
+                break;
+            }
+
+            usleep(100 * 1000); // 100ms
+        }
+
+        if (timedOut)
+        {
+            user_function();
+        }
+    });
+	notify_notification_show(notification, NULL);
+
+    gtk_main();
+    timerThread.join();
 	notify_uninit();
+
 	return 0;
 }
-
-//#include <libnotifymm.h>
-//
-//int main()
-//{
-//	Notify::init("Hello world!");
-//	Notify::Notification Hello("Hello world", "This is an example notification.", "dialog-information");
-//	Hello.show();
-//	return 0;
-//}
