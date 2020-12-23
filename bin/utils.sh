f0b7149106629ee034f808baff2727694db8a3d6
James Blades <jwkblades+git@gmail.com>
2020-12-23 09:50:33 -0700

    Split out shell utility functions

---
+++ bin/utils.sh
@@ -0,0 +1,44 @@
+###
+# NOTE: This file is meant to be sourced by other scripts and isn't executable
+#       on its own.
+###
+
+log()
+{
+    echo "${@}" >&2
+}
+
+warning()
+{
+    log "$(tput setaf 3)$(tput bold)WARNING:$(tput sgr0)$(tput setaf 3)" ${@}
+}
+
+error()
+{
+    log "$(tput setaf 1)$(tput bold)ERROR:$(tput sgr0)$(tput setaf 1)" ${@}
+}
+
+die()
+{
+    error ${@}
+    exit 1
+}
+
+exists()
+{
+    which ${1} &>/dev/null
+    return $?
+}
+
+requires()
+{
+    local ret=0
+    local app=""
+    for app in ${@}; do
+        if ! exists ${app}; then
+            error "Unable to find required application '${app}'"
+            ret=1
+        fi
+    done
+    return ${ret}
+}
