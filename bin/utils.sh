2c32feae20994b44fbabda0cbb5d496478fda403
James Blades <jwkblades+git@gmail.com>
2020-12-23 10:42:05 -0700

    Add ticker and update oci-build-container to use it

--- bin/utils.sh
+++ bin/utils.sh
@@ -3,33 +3,42 @@
 #       on its own.
 ###
 
+# Log a message to stderr
 log()
 {
     echo "${@}$(tput sgr0)" >&2
 }
 
+# Log a warning to stderr, colored yellow
 warning()
 {
     log "$(tput setaf 3)$(tput bold)WARNING:$(tput sgr0)$(tput setaf 3)" ${@}
 }
 
+# Log an error to stderr, colored red
 error()
 {
     log "$(tput setaf 1)$(tput bold)ERROR:$(tput sgr0)$(tput setaf 1)" ${@}
 }
 
+# Log an error, and exit with bad status
 die()
 {
     error ${@}
     exit 1
 }
 
+# Check if a command exists
 exists()
 {
     which ${1} &>/dev/null
     return $?
 }
 
+# Specify a list of commands that must exist, or the program can't run. The
+# entire list is checked, and each missing application is printed.
+#
+# NOTE: Doesn't automatically exit the script
 requires()
 {
     local ret=0
@@ -41,4 +50,49 @@ requires()
         fi
     done
     return ${ret}
+}
+
+# Runs a command in a ticker, capturing its output and hiding it behind a
+# single line. If the command successfully runs, then a checkbox is printed,
+# otherwise a red X is printed.
+ticker()
+{
+    local cmd="${1}"
+    shift
+
+    (
+        trap - ERR
+
+        local start=${SECONDS}
+        local current=${start}
+        local delta=0
+        local cols=${COLUMNS}
+
+        while [[ 0 == 0 ]]; do
+            cols=${COLUMNS}
+            current=${SECONDS}
+            delta=$(( ${current} - ${start} ))
+
+            printf "$(tput setaf 248)$(tput bold)[ ]$(tput sgr0) %-$(( ${cols} - 25 ))s $(tput bold)[%02d:%02d:%02d]\r$(tput sgr0)" "${@}" $(( ${delta} / 3600 )) $(( (${delta} % 3600) / 60 )) $(( ${delta} % 60 ))
+            sleep 1
+        done
+    ) &
+    local tickerPid=$!
+
+    local outFile="$(mktemp "/tmp/out-XXXXXX")"
+    local errFile="$(mktemp "/tmp/err-XXXXXX")"
+
+    ${cmd} 1>"${outFile}" 2>"${errFile}"
+    local rc=$?
+
+    kill ${tickerPid} 2>/dev/null
+    wait ${tickerPid}
+
+    if [[ ${rc} -eq 0 ]]; then
+        echo "$(tput setaf 2)$(tput bold)[✓]$(tput sgr0)"
+    else
+        echo "$(tput setaf 1)$(tput bold)[✘]$(tput sgr0)"
+        echo "    StdOut: ${outFile}"
+        echo "    StdErr: ${errFile}"
+    fi
 }
