f4088255ab8cee9acf2c5c67d937a38e6b4d6f7c
James Blades <jwkblades+git@gmail.com>
2020-12-24 16:44:24 -0700

    Comments for Application

--- src/components/i/Application.hpp
+++ src/components/i/Application.hpp
@@ -24,19 +24,60 @@
 
 #include <thread>
 
+/**
+ * Main application class
+ */
 class Application
 {
 public:
+    /**
+     * Construct an application
+     *
+     * @param argc The count of arguments being passed in
+     * @param argv The arguments themselves
+     */
     Application(int argc, char** argv);
+
+    /**
+     * Destructor
+     */
     ~Application(void);
 
+    /**
+     * Get the application global instance, set up by the first Application to
+     * be constructed.
+     *
+     * @param self The instance
+     * @return self (or, the previously set instance), only the first instance
+     *     set succeeds.
+     */
     static Application* instance(Application* self = NULL);
 
+    /**
+     * Callback used when we are told to close (by a SIGHUP, or SIGRTMIN)
+     */
     void closeSignal(void) const;
+
+    /**
+     * Callback used when the first option signal is sent (SIGRTMIN+1)
+     */
     void closeFirstOption(void) const;
+
+    /**
+     * Callback used when the second option signal is sent (SIGRTMIN+2)
+     */
     void closeSecondOption(void) const;
+
+    /**
+     * Callback used when the third option signal is sent (SIGRTMIN+3)
+     */
     void closeThirdOption(void) const;
 
+    /**
+     * The main application loop, doesn't return until the application is done.
+     *
+     * @return The return code of the application
+     */
     int main(void);
 private:
     bool mReady;
